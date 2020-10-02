56b02104ae4536a205d4036eb2c9c1defe9d850c
xScoopy <JLeary070@gmail.com>
2020-10-01 17:47:03 -0700

    completed second function, and tested functionality in browser

--- scripts.js
+++ scripts.js
@@ -15,10 +15,10 @@ hrs_per_day_input.addEventListener('input', calculateLength)
 unit_input.addEventListener('input', calculateLength)
 
 //adding event listeners for second function
-// total_episodes_input.addEventListener('input', function2)
-// avg_length_input.addEventListener('input', function2)
-// hrs_per_day_input.addEventListener('input', function2)
-// unit_input.addEventListener('input', function2)
+total_episodes_input.addEventListener('input', calculateTimeToWatch)
+avg_length_input.addEventListener('input', calculateTimeToWatch)
+hrs_per_day_input.addEventListener('input', calculateTimeToWatch)
+unit_input.addEventListener('input', calculateTimeToWatch)
 
 
 
@@ -41,3 +41,16 @@ function calculateLength() {
     
 }
 //defining episodes per day calculation function
+function calculateTimeToWatch() {
+    const total_episode_value = parseFloat(total_episodes_input.value)
+    const avg_length_value = parseFloat(avg_length_input.value)
+    const hrs_per_day_value = parseFloat(hrs_per_day_input.value)
+    const show_total_length = total_episode_value * avg_length_value
+    const hours_total = show_total_length / 60
+    const days_of_watching = hours_total / hrs_per_day_value
+    const weeks_to_finish = (days_of_watching / 7).toFixed(0)
+    const days_to_finish = (days_of_watching % 7).toFixed(0) 
+    const episodes_per_day = (hrs_per_day_value * 60) / avg_length_value
+    per_day_calc_display.innerHTML = 'If you watch ' + episodes_per_day + ' episodes per day, you will finish in ' + weeks_to_finish + ' week(s) and ' + days_to_finish + ' day(s).' 
+
+}
